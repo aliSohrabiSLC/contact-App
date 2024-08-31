ce7af824bac838ceb75563a9ef6f53286eda1bb2
Ali Sohrabi <alisohrabislc@gmail.com>
2024-08-31 21:33:03 +0330

    resolved dirty code and make folder like a constants and write inputs code in javaScript file

--- contactApp/src/components/Contacts.jsx
+++ contactApp/src/components/Contacts.jsx
@@ -2,8 +2,18 @@ import { useState } from "react";
 
 import ContactsList from "./ContactsList";
 
+import inputs from "../constants/inputs";
+
+const inputs = [
+  { type: "text", name: "name", placeholder: "Name" },
+  { type: "text", name: "lastname", placeholder: "Last Name" },
+  { type: "email", name: "email", placeholder: "Email" },
+  { type: "number", name: "phone", placeholder: "Phone" },
+];
+
 function Contacts() {
   const [contacts, setContacts] = useState([]);
+  const [alert, setAlert] = useState("");
   const [contact, setContact] = useState({
     name: "",
     lastName: "",
@@ -19,6 +29,16 @@ function Contacts() {
   };
 
   const addHandler = () => {
+    if (
+      !contact.name ||
+      !contact.lastName ||
+      !contact.email ||
+      !contact.phone
+    ) {
+      setAlert("Please enter valid data!");
+      return;
+    }
+    setAlert("");
     setContacts((contacts) => [...Contacts, contact]);
     setContact({
       name: "",
@@ -31,37 +51,21 @@ function Contacts() {
   return (
     <div>
       <div>
-        <input
-          type="text"
-          placeholder="Name"
-          name="name"
-          value={contact.name}
-          onChange={changeHandler}
-        />
-        <input
-          type="text"
-          placeholder="Last Name"
-          name="lastName"
-          value={contact.lastName}
-          onChange={changeHandler}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          value={contact.email}
-          onChange={changeHandler}
-        />
-        <input
-          type="number"
-          placeholder="Phone"
-          name="phone"
-          value={contact.phone}
-          onChange={changeHandler}
-        />
+        {inputs.map((input, index) => (
+          <input
+            key={index}
+            type={input.type}
+            placeholder={input.placeholder}
+            name={input.name}
+            value={contact[input.name]}
+            onChange={changeHandler}
+          />
+        ))}
+
         <button onClick={addHandler}>Add Contact</button>
       </div>
-      <ContactsList  contacts = {contacts}/>
+      <div>{alert && <p>{alert} </p>}</div>
+      <ContactsList contacts={contacts} />
     </div>
   );
 }
